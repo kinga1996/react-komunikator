8996f04a51040886a49229382f5655bd650f10a6
Kinga Kalisz <kingakalisz@gmail.com>
2020-10-23 15:10:37 +0200

    new final commit

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -5,6 +5,11 @@ import "./styl.css";
 function convertDate(timestamp) {
   const datetime = new Date(timestamp);
 
+  let day = datetime.getDate();
+  let month = datetime.getMonth();
+  let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let year = datetime.getFullYear();
+
   let hours = datetime.getHours();
   let min = datetime.getMinutes();
   let sec = datetime.getSeconds();
@@ -12,7 +17,7 @@ function convertDate(timestamp) {
   if (min < 10) {min = "0"+min;}
   if (sec < 10) {sec = "0"+sec;}
 
-  return (hours+':'+min+':'+sec);
+  return (day+' '+ monthNames[month] +' '+ year+ ' - ' + hours +':'+ min +':'+ sec);
 }
 
 function Message({ message }) {
--- src/components/MessageForm/index.js
+++ src/components/MessageForm/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import "./styl.css";
+
 function MessageForm({user, message, handleSubmit, handleContentChange, handleUserNameChange}){
     return(
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="user" value={user} onChange={handleUserNameChange} placeholder="Nick"/>
-                <input type="text" name="content" value={message}
-                onChange={handleContentChange} placeholder="Wiadomość"/>
+                <input id="name" type="text" name="user" value={user} onChange={handleUserNameChange} placeholder="Nick"/>
+                <input id="text" type="text" name="content" value={message} onChange={handleContentChange} placeholder="Message"/>
                 <input type="submit" value="Submit" />
             </form>
         </div>
