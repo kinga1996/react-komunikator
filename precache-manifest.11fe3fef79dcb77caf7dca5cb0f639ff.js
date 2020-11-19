e46a3f77530ae07c634a941983069bcada6ed5e0
Kinga Kalisz <kingakalisz@gmail.com>
2020-11-19 17:53:04 +0100

    Updates

---
+++ precache-manifest.11fe3fef79dcb77caf7dca5cb0f639ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536ce091bc5046f8a2d37ead6d04debe",
+    "url": "/react-messeger/index.html"
+  },
+  {
+    "revision": "baf1a7a84b7a43f56ed7",
+    "url": "/react-messeger/static/css/main.7a14f5b6.chunk.css"
+  },
+  {
+    "revision": "2896c394e069010bf993",
+    "url": "/react-messeger/static/js/2.b3d42c46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-messeger/static/js/2.b3d42c46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf1a7a84b7a43f56ed7",
+    "url": "/react-messeger/static/js/main.2546bb73.chunk.js"
+  },
+  {
+    "revision": "4387204ba048aab24484",
+    "url": "/react-messeger/static/js/runtime-main.5853c6c0.js"
+  }
+]);
